8fcbb509927db7b24afda09756f468113a62e859
Daniel Nordahl <danielnordahl93@gmail.com>
2019-04-11 01:46:12 +0200

    Updated Player Jump With Better Physics and Double Jump

--- Game-Programming-Project/Assets/Scripts/PlayerController.cs
+++ Game-Programming-Project/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    public float movementSpeed;
+    public float groundMovementSpeed;
+    public float airMovementSpeed;
     public float smoothing;
+
+    [Header("Jump")]
     public float jumpPower;
+    public float jumpTime = 1;
+
+    [Header("Fall")]
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
 
     [Header("Layers")]
     public LayerMask groundLayer;
@@ -18,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
     private bool facingRight;
     private bool grounded;
-    private bool jump;
+    private bool doubleJump;
     private float x;
 
     void Start()
@@ -30,27 +37,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        x = Input.GetAxis("Horizontal" + " " + gameObject.name) * movementSpeed;
-        if (Input.GetButtonDown("Jump" + " " + gameObject.name) && grounded) jump = true;
+        x = Input.GetAxis("Horizontal" + " " + gameObject.name) * (grounded ? groundMovementSpeed : airMovementSpeed);
+        if (Input.GetButtonDown("Jump" + " " + gameObject.name))
+        {
+            if (grounded || doubleJump) StartCoroutine(JumpRoutine());
+        }
 
         grounded = Physics2D.OverlapBox(transform.position, new Vector3(0.8f, 0.1f, 0), 0, groundLayer);
         anim.SetBool("IsJumping", !grounded);
+
+        if (!doubleJump && grounded) doubleJump = true;
     }
 
     private void FixedUpdate()
     {
         Movement();
-        Jump();
-    }
-
-    private void Jump()
-    {
-        if (jump)
-        {
-            anim.SetBool("IsJumping", true);
-            rb.AddForce(new Vector2(0f, jumpPower));
-            jump = false;
-        }
+        Fall();
     }
 
     public void Movement()
@@ -59,6 +61,33 @@ public class PlayerController : MonoBehaviour
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smoothing);
         if (x < 0 && facingRight || x > 0 && !facingRight) facingRight = Flip();
         anim.SetFloat("Speed", Mathf.Abs(x));
+    }
+
+    private void Fall()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump" + " " + gameObject.name))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    IEnumerator JumpRoutine()
+    {
+        rb.velocity = Vector2.zero;
+        float timer = 0;
+
+        while (Input.GetButton("Jump" + " " + gameObject.name) && timer < jumpTime)
+        {
+            float velocityY = Mathf.Sqrt(jumpPower * Mathf.Abs(Physics2D.gravity.y));
+            rb.velocity = new Vector2(rb.velocity.x, velocityY);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private bool Flip()
