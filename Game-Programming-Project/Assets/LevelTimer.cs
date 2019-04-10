055c428a1c6df3c2e1ee1c0e5587c4ac713d951f
Daniel Nordahl <danielnordahl93@gmail.com>
2019-04-10 19:39:07 +0200

    Fixed Camera Shakes

---
+++ Game-Programming-Project/Assets/LevelTimer.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI textField;
+
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            textField.text = "" + (int)timer;
+        }
+    }
+}
--- Game-Programming-Project/Assets/Scripts/Main.cs
+++ Game-Programming-Project/Assets/Scripts/Main.cs
@@ -1,17 +1,10 @@
-﻿using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-    public float timer;
-    public TextMeshProUGUI textField;
-
     void Update()
     {
-        timer -= Time.deltaTime;
-        textField.text = "" + (int)timer;
-
         if (Input.GetButtonDown("Restart")) SceneManager.LoadScene("Main");
     }
 }
