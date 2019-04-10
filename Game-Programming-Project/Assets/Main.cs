814dc1f1d1ab04915071d1b9016bc4472e539404
Daniel Nordahl <danielnordahl93@gmail.com>
2019-04-10 17:26:16 +0200

    Added Basic Timer and Restart Function

---
+++ Game-Programming-Project/Assets/Main.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public float timer;
+    public TextMeshProUGUI textField;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        textField.text = "" + (int)timer;
+
+        if (Input.GetButtonDown("Restart")) SceneManager.LoadScene("Main");
+    }
+}
