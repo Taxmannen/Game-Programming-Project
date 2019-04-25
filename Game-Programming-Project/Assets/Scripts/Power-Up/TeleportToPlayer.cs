fe76314b14e720a9b8514b2db2507d22efc46317
Daniel Nordahl <danielnordahl93@gmail.com>
2019-04-25 14:22:46 +0200

    Teleport Now Has A Working Effect

--- Game-Programming-Project/Assets/Scripts/Power-Up/TeleportToPlayer.cs
+++ Game-Programming-Project/Assets/Scripts/Power-Up/TeleportToPlayer.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 public class TeleportToPlayer : Item
 {
+    public float dissolveSpeed = 0.5f;
+    public float edgeSpeed = 0.2f;
+
     private Material mat;
     private PlayerController pc;
-    private Rigidbody2D rb;
+    private Coroutine coroutine;
 
     private void Start()
     {
-        mat = player.GetComponent<Material>();
-        rb = player.GetComponent<Rigidbody2D>();
+        mat = player.GetComponent<SpriteRenderer>().material;
         pc = player.GetComponent<PlayerController>();
         UseItem();
     }
@@ -22,24 +24,37 @@ public class TeleportToPlayer : Item
 
     private IEnumerator Teleport()
     {
-        //Play Effect;
-        bool startDissolve = true;
-        bool endDissolve = true;
-
         pc.SetUnableToMove(true);
-        while (startDissolve)
-        {
-            startDissolve = false;
-        }
+        StartCoroutine(LerpAndChangeShader(0.25f, edgeSpeed, "_Edges"));
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(edgeSpeed);
+
+        StartCoroutine(LerpAndChangeShader(1, dissolveSpeed, "_Level"));
+
+        yield return new WaitForSeconds(dissolveSpeed);
 
         player.position = otherPlayer.position;
-        while (endDissolve)
-        {
-            endDissolve = false;
-        }
+        StartCoroutine(LerpAndChangeShader(0f, edgeSpeed, "_Edges"));
+
+        yield return new WaitForSeconds(edgeSpeed);
+   
+        StartCoroutine(LerpAndChangeShader(0, dissolveSpeed, "_Level"));
+
+        yield return new WaitForSeconds(dissolveSpeed);
+
         pc.SetUnableToMove(false);
         base.UseItem();
+    }
+
+    private IEnumerator LerpAndChangeShader(float newValue, float fadeTime, string name)
+    {
+        float value = mat.GetFloat(name);
+        for (float time = 0.0f; time < 1.0f; time += Time.deltaTime / fadeTime)
+        {
+            float newColor = Mathf.Lerp(value, newValue, time);
+            mat.SetFloat(name, newColor);
+            yield return null;
+        }
+        mat.SetFloat(name, newValue);
     }
 }
