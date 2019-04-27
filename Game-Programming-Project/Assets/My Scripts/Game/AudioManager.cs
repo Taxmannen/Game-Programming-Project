7d8c67a9fc1ed584229d46fe1f8c254d5c692996
Daniel Nordahl <danielnordahl93@gmail.com>
2019-04-27 15:01:00 +0200

    Basic AudioManager with Sounds

---
+++ Game-Programming-Project/Assets/My Scripts/Game/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (Instance != null) return;
+        else Instance = this;
+    }
+
+    public void Play(string name, float volume = 1, float pitch = 1)
+    {
+        AudioClip audioClip = Resources.Load<AudioClip>("Sounds/" + name);
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        audioSource.Play();
+
+        Destroy(audioSource, audioClip.length * 2); 
+    }
+
+    public static AudioManager Instance { get; private set; }
+}
