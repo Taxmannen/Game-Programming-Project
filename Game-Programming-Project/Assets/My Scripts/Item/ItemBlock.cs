b4fd818eb64d9031fdd11980bfa40808be85a488
Daniel Nordahl <danielnordahl93@gmail.com>
2019-05-02 23:38:21 +0200

    Fixed Item Bug

--- Game-Programming-Project/Assets/My Scripts/Item/ItemBlock.cs
+++ Game-Programming-Project/Assets/My Scripts/Item/ItemBlock.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 public class ItemBlock : MonoBehaviour
 {
     #region Variables
-
-    [Header("Respawnable")]
-    [SerializeField] private float timeToSpawn;
-    [SerializeField] private bool respawnable;
-    [SerializeField] private float respawnTime;
+    [Header("Item Block")]
+    [SerializeField] protected float timeToSpawn;
 
     [Header("Debug")]
-    [SerializeField] private bool debugMode;
-    [SerializeField] private string itemName;
+    /*[SerializeField]**/ private string itemName = "";
 
-    private Coroutine coroutine;
-    private Collider2D[] colliders;
-    private SpriteRenderer sr;
-    private float spawnSpeed = 0.5f;
+    protected Coroutine coroutine;
+    protected Collider2D[] colliders;
+    protected SpriteRenderer sr;
+
+    protected float spawnSpeed = 0.5f;
 
     #endregion
 
@@ -25,10 +22,7 @@ public class ItemBlock : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         colliders = GetComponents<Collider2D>();
-
         SetColliders(false);
-
-        if (debugMode) Invoke("Spawn", timeToSpawn);  
     }
 
     public void ShowBlock()
@@ -41,13 +35,7 @@ public class ItemBlock : MonoBehaviour
         coroutine = StartCoroutine(FadeTo(1f, spawnSpeed));
     }
 
-    private void SetColliders(bool state)
-    {
-        foreach (Collider2D c in colliders) c.enabled = state;
-        if (!state) sr.color = new Color(1, 1, 1, 0);
-    }
-
-    private IEnumerator FadeTo(float newAlpha, float fadeTime)
+    protected IEnumerator FadeTo(float newAlpha, float fadeTime)
     {
         float alpha = sr.color.a;
         for (float time = 0.0f; time < 1.0f; time += Time.deltaTime / fadeTime)
@@ -63,25 +51,16 @@ public class ItemBlock : MonoBehaviour
         coroutine = null;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    protected void SetColliders(bool state)
     {
-        if (other.tag == "Player")
-        {
-            AudioManager.INSTANCE.Play("Item Box");
-            SpawnItem(other.transform);
-
-            if (!respawnable) Destroy(gameObject);
-            else
-            {
-                if (coroutine != null) StopCoroutine(coroutine);
-                SetColliders(false);
-                Invoke("Respawn", respawnTime);
-            }
-        }
+        foreach (Collider2D c in colliders) c.enabled = state;
+        if (!state) sr.color = new Color(1, 1, 1, 0);
     }
 
-    private void SpawnItem(Transform player)
+    protected void SpawnItem(Transform player)
     {
+        SetColliders(false);
+        AudioManager.INSTANCE.Play("Item Box");
         if (itemName.Length == 0)
         {
             PlayerStats playerStats = player.GetComponent<PlayerStats>();
@@ -101,8 +80,12 @@ public class ItemBlock : MonoBehaviour
         else player.GetComponent<PlayerInventory>().AddItem(Resources.Load<GameObject>("Items/" + itemName + " " + "Item"));
     }
 
-    private void Respawn()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        coroutine = StartCoroutine(FadeTo(1f, spawnSpeed));
+        if (other.CompareTag("Player"))
+        {
+            SpawnItem(other.transform);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Game-Programming-Project/Assets/My Scripts/RespawnableItemBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnableItemBlock : ItemBlock
+{
+    [Header("Respawnable")]
+    [SerializeField] private float respawnTime;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        colliders = GetComponents<Collider2D>();
+        SetColliders(false);
+        Invoke("Spawn", timeToSpawn);
+    }
+
+    private void Respawn()
+    {
+        coroutine = StartCoroutine(FadeTo(1f, spawnSpeed));
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpawnItem(other.transform);
+            if (coroutine != null) StopCoroutine(coroutine);
+            Invoke("Respawn", respawnTime);
+        }
+    }
+}
